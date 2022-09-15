9eb76f6926b3ea9b27df123e357ede947a0d6231
Douglas <douglas.wdias@hotmail.com>
2022-09-15 07:45:28 -0300

    Alt: images url

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,9 +5,14 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    fontFamily:{
+      sans: ['inter', 'sans-serif']
+    },
     extend: {
       backgroundImage:{
-        galaxy: "url('/background.png')"
+        galaxy: "url('/background.png')",
+        "nlw-gradient": "linear-gradient(89.86deg, #9572FC 5%, #43E7AD 65%, #E1D55D 95%)",
+        "game-gradient": "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)"
       }
     },
   },
